4fbd668c855640cf842f5e6c7da37f43b8ab9d5b
Pito Salas <pitosalas@gmail.com>
2014-05-28 17:16:26 -0400

    first commit

---
+++ cg_config.rb
@@ -0,0 +1,14 @@
+# Copyright string
+COPYRIGHT_STRING = "Copyright (2014) Tim Hickey, Marie Meteer, Pito Salas"
+
+# Course short name
+COURSE_SHORT_NAME = "JBS 2014 Cosi"
+COURSE_LONG_NAME = "... more detail ...."
+
+# Sections in the right hand margin of the page
+SECTION_CONFIG = [
+    SectionDef.new("Lecture Notes", "lecture_notes", type: :section),
+    SectionDef.new("Background", "background", type: :section),
+    SectionDef.new("Previous Years", "previous_years", type: :section)
+]
+
